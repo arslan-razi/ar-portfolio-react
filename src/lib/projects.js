99995a70a59b787f9bb1dc4cb623c12a64744df1
Arslan Razi <arslan.razi@outlook.com>
2022-03-16 14:44:37 -0400

    updated the projects

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -22,8 +22,8 @@ export const projects = [
         text: 'Password Generator, my first assignment with JavaScript.',
         technologies: 'JavaScript',
         deployed: true,
-        link: 'https://krutipatel07.github.io/password-generatore/',
-        github: 'https://github.com/krutipatel07/password-generatore',
+        link: 'https://arslan-razi.github.io/password-generatore/',
+        github: 'https://github.com/arslan-razi/password-generatore',
         description: 'The Password Generator was my first JavaScript assignment.  Upon clicking "Generate Password," the user is given a series of prompts to design the password composition.  A pool of possible password characters is created based off of user response, and the password is generated with Math.random().'
     }, 
     {
@@ -32,8 +32,8 @@ export const projects = [
         text: 'JavaScript Code Quiz, an interactive quiz that implements DOM manipulation.',
         technologies: 'JavaScript, HTML, CSS',
         deployed: true,
-        link: 'https://krutipatel07.github.io/code-quiz/',
-        github: 'https://github.com/krutipatel07/code-quiz',
+        link: 'https://arslan-razi.github.io/code-quiz/',
+        github: 'https://github.com/arslan-razi/code-quiz',
         description: 'This quick and simple JavaScript assignment dynamically creates DOM elements to render questions and answers.  Scores are saved using local storage and can be viewed on the ranked scoreboard.'
     }, 
     {
@@ -42,8 +42,8 @@ export const projects = [
         text: 'Work Day Scheduler, an productivity application that utilizes JQuery and Bootstrap.',
         technologies: 'JQuery, Moment.js, Bootstrap, HTML, CSS',
         deployed: true,
-        link: 'https://krutipatel07.github.io/Work-Day-Scheduler/',
-        github: 'https://github.com/krutipatel07/Work-Day-Scheduler',
+        link: 'https://arslan-razi.github.io/Work-Day-Scheduler/',
+        github: 'https://github.com/arslan-razi/Work-Day-Scheduler',
         description: 'Built with JQuery and Bootstrap, this Work Day Scheduler allows users to plan their 9 to 5 day.  The scheduler is updated in real time using Moment.js with the current time block being highlighted in red and future time blocks displayed in green.'
     },
     {
@@ -52,8 +52,8 @@ export const projects = [
         text: 'Weather Dashboard, a weather application using the OpenWeather API.',
         technologies: 'Open Weather API, Moment.js, JavaScript, Bootstrap, HTML',
         deployed: true,
-        link: 'https://krutipatel07.github.io/Weather-Dashboard/',
-        github: 'https://github.com/krutipatel07/Weather-Dashboard',
+        link: 'https://arslan-razi.github.io/Weather-Dashboard/',
+        github: 'https://github.com/arslan-razi/Weather-Dashboard',
         description: 'This weather application uses the OpenWeather API to display the daily and 5-day forecast of the user searched U.S. city.  Up to five of the most recently searched cities are saved.'
     }, 
     {
@@ -62,7 +62,7 @@ export const projects = [
         text: 'README Generator, a Node.js application to generate a professional README with Inquirer.',
         technologies: 'Node.js, Inquirer.js, JavaScript',
         deployed: false,
-        github: 'https://github.com/krutipatel07/README-generator',
+        github: 'https://github.com/arslan-razi/README-generator',
         description: 'The README Generator is a Node.js application built with Inquirer.js.  The README Generator a professional README markdown file based on user input for desired sections.'
     }, 
     {
@@ -71,7 +71,7 @@ export const projects = [
         text: 'Team Profile Generator, an application for creating a team roster using Node.js and Inquirer.',
         technologies: 'Node.js, Inquirer.js, Jest, JavaScript',
         deployed: false,
-        github: 'https://github.com/krutipatel07/TeamProfileGenerator',
+        github: 'https://github.com/arslan-razi/TeamProfileGenerator',
         description: 'The Team Profile Generator was an exercise in object-oriented programming and test-driven developement.  The application itself runs in Node.js and uses Inquirer.js to create a simple webpage featuring a development team\'s roster.  Tests for the application were written using Jest.'
     }, 
     {
@@ -81,7 +81,7 @@ export const projects = [
         technologies: 'Express.js, Node.js, UUID, JavaScript',
         deployed: true,
         link: 'https://express-js-challenge.herokuapp.com/',
-        github: 'https://github.com/krutipatel07/Note-Taker',
+        github: 'https://github.com/arslan-razi/Note-Taker',
         description: 'Note Taker was my first Express.js assignment.  It uses POST and DELETE routes to create, edit, and delete notes from the db.json file.'
     }, 
     {
@@ -90,7 +90,7 @@ export const projects = [
         text: 'Employee Tracker, an application that allows business owners to manage employees.',
         technologies: 'MySQL, Node.js, Inquirer.js, JavaScript',
         deployed: false,
-        github: 'https://github.com/krutipatel07/Employee-Tracker',
+        github: 'https://github.com/arslan-razi/Employee-Tracker',
         description: 'Employee Tracker uses MySQL to track employees and their roles, departments, and salaries within a company.  The application was built in Node.js and uses Inquirer.js to allow the user to interact with the database.'
     }, 
     {
@@ -99,7 +99,7 @@ export const projects = [
         text: 'e-Commerce Back End, the back-end of an e-commerce site.',
         technologies: 'MySQL, Sequelize, Node.js, Express.js, JavaScript',
         deployed: false,
-        github: 'https://github.com/krutipatel07/E-commerce',
+        github: 'https://github.com/arslan-razi/E-commerce',
         description: 'The e-Commerce Back End application was an exercise in REST APIs and CRUD operations.  The application does not have a front-end for public use, but API GET, POST, PUT, and DELETE routes are fully functional for maintaining a commerce database of products, product tags, and product categories.'
     },
     {
@@ -109,7 +109,7 @@ export const projects = [
         technologies: 'MySQL, Sequelize, Express.js, Express Sessions, Node.js, Bcrypt, Handlebars',
         deployed: true,
         link: 'https://quiet-escarpment-26240.herokuapp.com/',
-        github: 'https://github.com/krutipatel07/MVC-TechBlog',
+        github: 'https://github.com/arslan-razi/MVC-TechBlog',
         description: 'The Tech Blog is a REST API blog created with MySQL and Sequelize.  Users can create accounts to post to the blog or comment on existing posts.  A user\'s post and comment history can be viewed from their dashboard.'
     },
     {
@@ -118,7 +118,7 @@ export const projects = [
         text: 'Social Network API, a REST API built with MongoDB and Mongoose.',
         technologies: 'MongoDB, Mongoose, Express.js, Node.js',
         deployed: false,
-        github: 'https://github.com/krutipatel07/API-Social-Network',
+        github: 'https://github.com/arslan-razi/API-Social-Network',
         description: 'This Social Network API was a REST API exercise built with MongoDB and Mongoose.  It does not have a front-end for public use, but API GET, POST, PUT, and DELETE routes can be tested in clients such as Insomnia Core or Postman.'
     },
     {
@@ -128,7 +128,7 @@ export const projects = [
         technologies: 'IndexedDB, Service Workers, JavaScript',
         deployed: true,
         link: 'https://budget-tracker-pwa01.herokuapp.com/',
-        github: 'https://github.com/krutipatel07/PWA-Budget-Tracker',
+        github: 'https://github.com/arslan-razi/PWA-Budget-Tracker',
         description: 'The Budget Tracker is a progressive web application that uses IndexedDB and service workers to be fully functional offline.'
     }
 ];
